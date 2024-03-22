d37c2fbaae4acfb1bbe5205a81905677c2247547
brianna-l <loub697@newschool.edu>
2024-03-21 21:00:10 -0400

    up

---
+++ studio/02_elastic-collection/assets/object1--.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var div = document.getElementById('clickableDiv');
+    var images = ["/studio/02_elastic-collection/media/m1-.png", "/studio/02_elastic-collection/media/m1--.png", "/studio/02_elastic-collection/media/m1----.png", "/studio/02_elastic-collection/media/m1---.png"];
+    var currentIndex = 0;
+  
+    div.addEventListener('click', function() {
+      currentIndex = (currentIndex + 1) % images.length;
+      div.style.backgroundImage = "url('" + images[currentIndex] + "')";
+    });
+  });

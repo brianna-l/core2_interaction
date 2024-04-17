8bd32a3e29327a85df7065d2c88587c432405df9
brianna-l <loub697@newschool.edu>
2024-04-17 19:38:23 -0400

    up

--- lab/projects/workshop9/assets/main.js
+++ lab/projects/workshop9/assets/main.js
@@ -34,16 +34,16 @@ function render(data) {
 
     // edit info element
     infoElement.innerHTML = `
-        <h2>Level of PM2.5</h2>
+        <h2 style="color: #FF1347;">Current Level of PM2.5</h2>
         <h5><u>List of pollution elements</u></h5>
-        <p>CO (Carbon monoxide): ${ data.list[0].components.co } µg/m3</p>
-        <p>NO (Nitrogen monoxide): ${ data.list[0].components.no } µg/m3</p>
-        <p>NO2 (Nitrogen dioxide): ${ data.list[0].components.no2 } µg/m3</p>
-        <p>O3 (Ozone): ${ data.list[0].components.o3 } µg/m3</p>
-        <p>SO2 (Sulphur dioxide): ${ data.list[0].components.so2 } µg/m3</p>
-        <p>PM2.5 (Fine particles matter): ${ data.list[0].components.pm2_5 } µg/m3</p>
-        <p>PM10 (Coarse particulate matter): ${ data.list[0].components.pm10 } µg/m3</p>
-        <p>NH3 (Ammonia): ${ data.list[0].components.nh3 } µg/m3</p>
+        <p>CO <i style="color: #FF6347;">(Carbon monoxide)</i>: ${ data.list[0].components.co } µg/m3</p>
+        <p>NO <i style="color: #FF6347;">(Nitrogen monoxide)</i>: ${ data.list[0].components.no } µg/m3</p>
+        <p>NO2 <i style="color: #FF6347;">(Nitrogen dioxide)</i>: ${ data.list[0].components.no2 } µg/m3</p>
+        <p>O3 <i style="color: #FF6347;">(Ozone)</i>: ${ data.list[0].components.o3 } µg/m3</p>
+        <p>SO2 <i style="color: #FF6347;">(Sulphur dioxide)</i>: ${ data.list[0].components.so2 } µg/m3</p>
+        <p>PM2.5 <i style="color: #FF6347;">(Fine particles matter)</i>: ${ data.list[0].components.pm2_5 } µg/m3</p>
+        <p>PM10 <i style="color: #FF6347;">(Coarse particulate matter)</i>: ${ data.list[0].components.pm10 } µg/m3</p>
+        <p>NH3 <i style="color: #FF6347;">(Ammonia)</i>: ${ data.list[0].components.nh3 } µg/m3</p>
     
     `;
 

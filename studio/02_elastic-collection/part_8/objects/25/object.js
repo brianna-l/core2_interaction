6f772f38a518b6c55b448f1e1187ec04c2d3f70d
brianna-l <loub697@newschool.edu>
2024-05-09 19:57:28 -0400

    up

--- studio/02_elastic-collection/part_8/objects/25/object.js
+++ studio/02_elastic-collection/part_8/objects/25/object.js
@@ -1,20 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
   var div = document.getElementById('clickableDiv');
-  var shuffle = document.getElementById('shuffle');
   var images = ["/studio/02_elastic-collection/media/25-.png", "/studio/02_elastic-collection/media/25--.png", "/studio/02_elastic-collection/media/25---.png", "/studio/02_elastic-collection/media/25----.png"];
   var currentIndex = 0;
 
   div.addEventListener('click', function() {
     currentIndex = (currentIndex + 1) % images.length;
-    div.style.backgroundImage = "url('" + images[currentIndex] + "')";
-  });
-
-  clickableDiv.addEventListener('click', function() {
-    var randomIndex = Math.floor(Math.random() * images.length);
-    while(randomIndex === currentIndex) {
-      randomIndex = Math.floor(Math.random() * images.length);
-    }
-    currentIndex = randomIndex;
     div.style.backgroundImage = "url('" + images[currentIndex] + "')";
   });
 });
